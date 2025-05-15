764676ec97bd8fdff202add1a3f6fe31d652d293
cindy-prog <cindynjeri37@gmail.com>
2025-05-15 20:28:29 +0300

    week6 assignment

--- database assign/answers.sql
+++ database assign/answers.sql
@@ -1,12 +1,30 @@
--- write an sql query to drop an index named idxPhone from customers 
-DROP TABLE idxPhone 
-ON customers;
--- create user 'bob' with password ''S$cu3r3!' , restricted to the localhost hostname.
-CREATE USER 'bob' @ 'localhost'
- IDENTIFIED BY 'S$cu3r3!';
--- Write an SQL query to grant the INSERT privilege to the user bob on the salesDB database.
-GRANT INSERT ON salesDB, 
-* TO 'bob' @ 'localhost';
--- Write an SQL query to change the password for the user bob to 'P$55!23'
-ALTER USER 'bob' @ 'localhost'
-IDENTIFIED BY 'P$55!23';
+-- Get employee details with office information using INNER JOIN
+SELECT 
+    e.firstName, 
+    e.lastName, 
+    e.email, 
+    e.officeCode
+FROM 
+    employees e
+INNER JOIN 
+    offices o ON e.officeCode = o.officeCode;
+-- Get product details with product line information using LEFT JOIN
+SELECT 
+    p.productName, 
+    p.productVendor, 
+    p.productLine
+FROM 
+    products p
+LEFT JOIN 
+    productlines pl  ON p.productLine = pl.productLine;
+-- Get order details with customer information for first 10 orders using RIGHT JOIN
+SELECT 
+    o.orderDate, 
+    o.shippedDate, 
+    o.status, 
+    o.customerNumber
+FROM 
+    customers c
+RIGHT JOIN 
+    orders o ON c.customerNumber = o.customerNumber
+LIMIT 10;
